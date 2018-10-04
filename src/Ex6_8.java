160dd14620e4f8e2f4142fcf335a8a18c6cec43d
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-10-04 13:51:10 +0200

    Celcius to fahrenheit // output, input call method

---
+++ src/Ex6_8.java
@@ -0,0 +1,32 @@
+public class Ex6_8 {
+
+
+    //                     output                input
+    public static double celciusToFahrenheit(double celsius) {
+        double fahrenheit = 0;
+
+
+        fahrenheit = (9.0 / 5) * celsius + 32;
+        // double celsius = (5.0 / 9) * (fahrenheit –32);
+        return fahrenheit;
+    }
+
+    public static void main(String[] args) {
+        double x = celciusToFahrenheit(10);
+        System.out.println(x);
+
+        x = celciusToFahrenheit(100);
+        System.out.println(x);
+
+        double s = fahrenheitToCelsius(5);
+        System.out.println(s);
+    }
+    public static double fahrenheitToCelsius(double fahrenheit){
+        double celsius = 0;
+        celsius = (25/2) + fahrenheit + 35;
+        return celsius;
+
+
+    }
+
+}
