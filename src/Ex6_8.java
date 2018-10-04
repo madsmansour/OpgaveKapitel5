63a3c9bb74468cc897db14afbad8ca4ac67b9c60
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-10-04 14:18:33 +0200

    Celcius to fahrenheit // output, input call method

--- src/Ex6_8.java
+++ src/Ex6_8.java
@@ -28,5 +28,6 @@ public class Ex6_8 {
 
 
     }
+    
 
 }
---
+++ src/footToMeter.java
@@ -0,0 +1,23 @@
+public class footToMeter {
+
+    public static double footToMeter(double foot) {
+        double meter = 0;
+        //foot = 3.279 * meter;
+        meter = 0.305 * foot;
+        return meter;
+
+    }
+
+    public static void main(String[] args) {
+        double k = footToMeter(5);
+        System.out.println(k);
+        double p = meterToFoot(5);
+        System.out.println(p);
+    }
+
+    public static double meterToFoot(double meter) {
+        double foot = 0;
+        foot = 3.279 * meter;
+        return foot;
+    }
+}
